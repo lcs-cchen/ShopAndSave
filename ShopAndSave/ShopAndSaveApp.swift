a090fc48a7a5faf0fc0964041404161d465df6f9
cyrus chen <pschen@lcs.on.ca>
2023-06-01 14:40:53 -0400

    Created Folders for future files

--- ShopAndSave/ShopAndSaveApp.swift
+++ ShopAndSave/ShopAndSaveApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ShopAndSaveApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ShopAndSaveView()
         }
     }
 }
---
+++ ShopAndSave/Views/ShopAndSaveView.swift
@@ -0,0 +1,20 @@
+//
+//  ShopAndSaveView.swift
+//  ShopAndSave
+//
+//  Created by Cyrus Chen on 1/6/2023.
+//
+
+import SwiftUI
+
+struct ShopAndSaveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShopAndSaveView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShopAndSaveView()
+    }
+}
