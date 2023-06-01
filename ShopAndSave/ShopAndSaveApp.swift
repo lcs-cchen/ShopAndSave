b15af15c9e7cd1b4b7f3e0d7817f7dc19f1462fa
cyrus chen <pschen@lcs.on.ca>
2023-06-01 14:38:03 -0400

    Initial Commit

---
+++ ShopAndSave/ShopAndSaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAndSaveApp.swift
+//  ShopAndSave
+//
+//  Created by Cyrus Chen on 1/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShopAndSaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
